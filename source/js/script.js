a1f4f1a9f7f6a3448e7418f5e6a958136b5d380a
Den <deneroxin6@gmail.ru>
2022-07-28 17:44:03 +0300

    completed automation

---
+++ source/js/script.js
@@ -0,0 +1,16 @@
+//main-nav
+
+const mainNav = document.querySelector(".main-nav");
+const mainNavButton = mainNav.querySelector(".main-nav__button-close");
+
+mainNav.classList.remove("main-nav--nojs");
+
+mainNavButton.addEventListener("click", function () {
+  if (mainNav.classList.contains("main-nav--closed")) {
+    mainNav.classList.remove("main-nav--closed");
+    mainNav.classList.add("main-nav--opened");
+  } else {
+      mainNav.classList.add("main-nav--closed");
+      mainNav.classList.remove("main-nav--opened");
+      }
+    })
